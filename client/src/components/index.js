e499835d60a4e902ca0723389bd902021d0d5d61
GudeUmaMaheswari <99406459+GudeUmaMaheswari@users.noreply.github.com>
2024-12-18 15:59:19 +0530

    Delete client directory

--- client/src/components/index.js
+++
@@ -1,4 +0,0 @@
-import Card from './Card';
-import Formfield from './Formfield';
-import Loader from './Loader';
-export { Card , Formfield , Loader};
--- client/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import Home from "./Home";
-import CreatePost from "./CreatePost";
-import LoginForm from "./LoginForm";
-import SignupForm from "./SignupForm";
-export {Home,CreatePost,LoginForm,SignupForm};
